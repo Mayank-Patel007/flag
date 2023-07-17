e5412995dc9c275e3d74a3c031aef226119bd0a0
Mayank-Patel007 <mayankpatel4149.com>
2023-07-17 23:30:10 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import data from './flag.jsx'
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const [swich, setSwich] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className="navbar">
+        
+
+      <p className="para">Where is the world?</p>
+      <div className="switch">
+       <div className="form-check form-switch" onClick={()=>{setSwich(!swich)}}>
+  <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" forHTML="flexSwitchCheckDefault">{swich?"Light Mode":"Dark Mode"}</label>
+</div>
+        </div>
+         
+         </nav>
+
+      <div className={`${swich ? "dark" : "white"}`}>
+        
+        {data.map((a) => (
+          <div className='box'>
+            <div className="image">
+              <img src={a.flags['png']} alt="" />
+            </div>
+            <h2>{a.name['common']}</h2>
+            <p>Population : {a.population}</p>
+            <p>Region : {a.region}</p>
+
+            <p>Capital : {a.capital}</p>
+          </div>
+
+        ))}
+
+      </div>
+    </>
+
   );
 }
 
